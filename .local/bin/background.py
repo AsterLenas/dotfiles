87fc64ec09ddcb41f82caac882792d07a23f361e
cinque <sinon.adastrum@gmail.com>
2024-04-23 18:53:47 +0200

    new wallpaper

--- .local/bin/background.py
+++ .local/bin/background.py
@@ -13,7 +13,7 @@ def filter(string, substring):
 home = expanduser("~")
 fixpath = home + "/nextcloud-backup/Wallpaper"
 imgdirs = ["others", "alphacoders", "elden_ring", "nobunaga", "nexus",
-           "cirno", "lodoss", "kancolle", "lies_of_p", "ships"]
+           "cirno", "lodoss", "kancolle", "lies_of_p", "ships", "eiyuden_chronicles"]
 imgpaths = [join(fixpath, p) for p in imgdirs]
 
 for imgpath in imgpaths:
